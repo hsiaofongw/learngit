6f0c5a7a012bafe556999b5ce6cc8b50a4131d89
hsiaofeng <hsiaofong.w@gmail.com>
2020-03-08 00:52:32 +0800

    added compare function by Sam

--- calc.py
+++ calc.py
@@ -7,3 +7,15 @@ def sub(x, y):
 
 def mul(x, y):
     return x * y
+
+# 比较两个整数x, y
+# 如果x < y 返回-1
+# 如果x = y 返回0
+# 如果x > y 返回1
+def compare(x, y):
+    if (x < y):
+        return -1
+    elif (x == y):
+        return 0
+    elif (x > y):
+        return 1
