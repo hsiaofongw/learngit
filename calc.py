d13e1f801bbbf8f6fb9f6ac6f2e12d9bc43e80d0
hsiaofeng <hsiaofong.w@gmail.com>
2020-03-07 23:30:05 +0800

    (c7) added div function

--- calc.py
+++ calc.py
@@ -1,9 +1,18 @@
+# author: david
+# last_modified: 2020-03-07T23:29:17+08:00
+
 # 加法
 def add(x, y):
     return x + y
 
+# 减法
 def sub(x, y):
     return x - y
 
+# 乘法
 def mul(x, y):
     return x * y
+
+# 除法
+def div(x, y):
+    return x / y
